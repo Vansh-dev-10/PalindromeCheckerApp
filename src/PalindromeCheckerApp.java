5afad2c72babd4153080ee9b5e3eaa8f4fbade4c
adx69 <ar6553@gmail.com>
2026-02-27 10:30:42 +0530

    UC6: Queue and Stack based palindrome check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,4 +1,6 @@
 import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class PalindromeCheckerApp {
 
@@ -102,11 +104,43 @@ public class PalindromeCheckerApp {
             reversedUsingStack = reversedUsingStack + stack.pop();
         }
 
-        // Compare original and reversed strings
         if (input3.equals(reversedUsingStack)) {
             System.out.println("UC5: The word \"" + input3 + "\" is a Palindrome.");
         } else {
             System.out.println("UC5: The word \"" + input3 + "\" is NOT a Palindrome.");
+        }
+
+        System.out.println();
+
+
+        // ================= UC6 =================
+        // Queue + Stack Based Palindrome Check
+        String input4 = "refer";
+
+        Queue<Character> queue = new LinkedList<>();
+        Stack<Character> stack2 = new Stack<>();
+
+        // Enqueue & Push characters
+        for (int i = 0; i < input4.length(); i++) {
+            char ch = input4.charAt(i);
+            queue.add(ch);     // FIFO
+            stack2.push(ch);  // LIFO
+        }
+
+        boolean isPalindromeQueueStack = true;
+
+        // Compare dequeue and pop
+        while (!queue.isEmpty()) {
+            if (queue.remove() != stack2.pop()) {
+                isPalindromeQueueStack = false;
+                break;
+            }
+        }
+
+        if (isPalindromeQueueStack) {
+            System.out.println("UC6: The word \"" + input4 + "\" is a Palindrome.");
+        } else {
+            System.out.println("UC6: The word \"" + input4 + "\" is NOT a Palindrome.");
         }
     }
 }
