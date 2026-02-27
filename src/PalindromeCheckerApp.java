a2e9b99a646c5662106fe9e9de3edaad77414b89
adx69 <ar6553@gmail.com>
2026-02-27 10:20:26 +0530

    UC2: Print hardcoded palindrome result

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,13 +2,35 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
+        // UC1 - Welcome Message
         System.out.println("===========================================");
         System.out.println("        WELCOME TO PALINDROME CHECKER       ");
         System.out.println("===========================================");
         System.out.println("Application Name    : Palindrome Checker App");
         System.out.println("Application Version : 1.0");
-        System.out.println("===========================================");
+        System.out.println("===========================================\n");
 
-        // Program will continue to next use cases
+        // UC2 - Hardcoded Palindrome Check
+        String word = "madam";   // Hardcoded string
+
+        boolean isPalindrome = true;
+
+        int start = 0;
+        int end = word.length() - 1;
+
+        while (start < end) {
+            if (word.charAt(start) != word.charAt(end)) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        if (isPalindrome) {
+            System.out.println("The word \"" + word + "\" is a Palindrome.");
+        } else {
+            System.out.println("The word \"" + word + "\" is NOT a Palindrome.");
+        }
     }
 }
