fceecafb77342b6ebaec274af50c25afd5f1a42e
adx69 <ar6553@gmail.com>
2026-02-27 10:14:31 +0530

    UC1: Application entry and welcome message

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,7 +1,14 @@
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
-        System.out.println("Palindrome Checker App");
-    }
 
+        System.out.println("===========================================");
+        System.out.println("        WELCOME TO PALINDROME CHECKER       ");
+        System.out.println("===========================================");
+        System.out.println("Application Name    : Palindrome Checker App");
+        System.out.println("Application Version : 1.0");
+        System.out.println("===========================================");
+
+        // Program will continue to next use cases
+    }
 }
