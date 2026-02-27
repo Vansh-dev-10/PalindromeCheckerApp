b34193dd0a53db56c1bdfabb3a15bec565b4a626
adx69 <ar6553@gmail.com>
2026-02-27 10:26:00 +0530

    UC4: Character array based palindrome check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,7 +2,8 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        // UC1 - Welcome Message
+        // ================= UC1 =================
+        // Application Entry & Welcome Message
         System.out.println("===========================================");
         System.out.println("        WELCOME TO PALINDROME CHECKER       ");
         System.out.println("===========================================");
@@ -10,10 +11,12 @@ public class PalindromeCheckerApp {
         System.out.println("Application Version : 1.0");
         System.out.println("===========================================\n");
 
-        // UC2 - Hardcoded Palindrome Check (Two Pointer Method)
-        String word = "madam";
 
+        // ================= UC2 =================
+        // Hardcoded Palindrome Check (Two Pointer Method)
+        String word = "madam";
         boolean isPalindrome = true;
+
         int start = 0;
         int end = word.length() - 1;
 
@@ -34,20 +37,47 @@ public class PalindromeCheckerApp {
 
         System.out.println();
 
-        // UC3 - Palindrome Check Using String Reverse
+
+        // ================= UC3 =================
+        // Palindrome Check Using String Reverse
         String input = "level";
         String reversed = "";
 
-        // Reverse string using for loop
         for (int i = input.length() - 1; i >= 0; i--) {
             reversed = reversed + input.charAt(i);
         }
 
-        // Compare original and reversed strings
         if (input.equals(reversed)) {
             System.out.println("UC3: The word \"" + input + "\" is a Palindrome.");
         } else {
             System.out.println("UC3: The word \"" + input + "\" is NOT a Palindrome.");
+        }
+
+        System.out.println();
+
+
+        // ================= UC4 =================
+        // Character Array Based Palindrome Check
+        String input2 = "radar";
+        char[] chars = input2.toCharArray();
+
+        int left = 0;
+        int right = chars.length - 1;
+        boolean isPalindromeCharArray = true;
+
+        while (left < right) {
+            if (chars[left] != chars[right]) {
+                isPalindromeCharArray = false;
+                break;
+            }
+            left++;
+            right--;
+        }
+
+        if (isPalindromeCharArray) {
+            System.out.println("UC4: The word \"" + input2 + "\" is a Palindrome.");
+        } else {
+            System.out.println("UC4: The word \"" + input2 + "\" is NOT a Palindrome.");
         }
     }
 }
